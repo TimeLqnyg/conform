860b8d04f68557ac7753c0a9193e80a8e0531f87
TimeLqnyg <2690125590@qq.com>
2019-09-10 21:57:14 +0800

    first commit

--- src/main/java/cn/zjh/conform/service/sercurity/MyUserDetailService.java
+++ src/main/java/cn/zjh/conform/service/sercurity/MyUserDetailService.java
@@ -1,29 +1,44 @@
 package cn.zjh.conform.service.sercurity;
 
+import cn.zjh.conform.dao.UserMapper;
+import cn.zjh.conform.model.Permission;
+import cn.zjh.conform.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class MyUserDetailService implements UserDetailsService {
 
+	@Autowired
+	private UserMapper userMapper;
+
 	/**
 	 * 读取用户信息
-	 * @param s
+	 * @param username
 	 * @return
 	 * @throws UsernameNotFoundException
 	 */
 	@Override
-	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 		//UserDetails 封装用户数据的接口
-		User user=new User("user",new BCryptPasswordEncoder().encode("123456"),
-				AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER,ROLE_ADMIN"));
+//		User user=new User("user",new BCryptPasswordEncoder().encode("123456"),
+//				AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER,ROLE_ADMIN"));
 
+		User user=userMapper.findByUsername(username);
+
+		List<Permission> list=userMapper.findPermissionByUsername(username);
+
+		List<GrantedAuthority> grantedAuthorityList=new ArrayList<>();
 
 		return user;
 	}
---
+++ src/main/java/cn/zjh/conform/utils/MyPasswordEncoder.java
@@ -0,0 +1,17 @@
+package cn.zjh.conform.utils;
+
+import org.apache.tomcat.util.security.MD5Encoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class MyPasswordEncoder implements PasswordEncoder {
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return rawPassword.toString();
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return false;
+	}
+}
