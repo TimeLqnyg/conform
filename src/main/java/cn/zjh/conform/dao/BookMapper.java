a1121d5b1e998085e16794cf9fe96a681a79a6ef
TimeLqnyg <2690125590@qq.com>
2019-09-23 10:35:16 +0800

    commit

--- src/main/java/cn/zjh/conform/dao/BookMapper.java
+++ src/main/java/cn/zjh/conform/dao/BookMapper.java
@@ -1,8 +1,7 @@
 package cn.zjh.conform.dao;
 
 import cn.zjh.conform.model.Book;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 //@Mapper
@@ -12,8 +11,12 @@ public interface BookMapper {
 	@Select("select * from book where id=#{id}")
 	Book queryBookById(Integer id);
 
-	@Select("select * from book where name=#{id}")
+	@Select("select * from book where name=#{bookName}")
 	Book getBookByName(String bookName);
+
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+	@Insert("insert into book(name) values (#{bookName})")
+	Integer addBook( @Param("bookName") String bookName);
 
 }
 
