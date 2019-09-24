e446e873182b7321ce01a5a9c3308334017d4b3b
TimeLqnyg <2690125590@qq.com>
2019-09-24 22:43:50 +0800

    commit

---
+++ src/main/java/cn/zjh/conform/service/RMI/remoteService.java
@@ -0,0 +1,10 @@
+package cn.zjh.conform.service.RMI;
+
+import cn.zjh.conform.model.Authorities;
+
+public interface remoteService {
+	void save(Authorities authorities);
+}
+
+
+//HttpInvokerServiceExporter
