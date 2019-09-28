55732a3c1451a38e239b76c00ccf4346721acdc6
TimeLqnyg <2690125590@qq.com>
2019-09-28 20:24:34 +0800

    commit

--- src/main/java/cn/zjh/conform/utils/MyPasswordEncoder.java
+++ src/main/java/cn/zjh/conform/utils/MyPasswordEncoder.java
@@ -6,12 +6,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class MyPasswordEncoder implements PasswordEncoder {
 
 	@Override
-	public String encode(CharSequence rawPassword) {
-		return rawPassword.toString();
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return false;
 	}
 
 	@Override
-	public boolean matches(CharSequence rawPassword, String encodedPassword) {
-		return false;
+	public String encode(CharSequence rawPassword) {
+		return rawPassword.toString();
 	}
 }
