f5768c5fedbd66e050fe21c6364e42dad0222420
TimeLqnyg <2690125590@qq.com>
2019-10-21 14:47:20 +0800

    rpc

---
+++ dubbo-demo/src/test/java/cn/zjh/dubbodemo/DubboDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zjh.dubbodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
